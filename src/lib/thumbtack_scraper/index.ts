855cce9f260ef3c84d11abee015b215f8aeab739
Brahm Van Houzen <brahmvanh@gmail.com>
2024-06-04 18:53:59 -0400

    catch

--- src/lib/thumbtack_scraper/index.ts
+++ src/lib/thumbtack_scraper/index.ts
@@ -1,16 +1,14 @@
-import { Review } from '../../generated/graphql';
+import { ThumbtackReview } from '../../generated/graphql';
 import { extractThumbtackData } from './helper';
 
-import { PythonShell } from 'python-shell';
 import { execSync } from 'child_process';
 
 // Scrape thumbtack data
-// I wasn't able to get the timing right for getting the data, so I 
+// I wasn't able to get the timing right for getting the data, so I
 // chose to just block the thread until the data is returned.
 const scrape = () => {
-
 	const scrapedData = execSync('python src/lib/thumbtack_scraper/thumbtack_scraper.py', { encoding: 'utf8' });
-  const jsonString = scrapedData.replace(/ '/g, ' "').replace(/' /g, '" ').replace(/{'/g, '{"').replace(/'}/g, '"}').replace(/':/g, '":').replace(/',/g, '",');
+	const jsonString = scrapedData.replace(/ '/g, ' "').replace(/' /g, '" ').replace(/{'/g, '{"').replace(/'}/g, '"}').replace(/':/g, '":').replace(/',/g, '",');
 	console.log('jsonString', jsonString);
 	const reviewJson = JSON.parse(jsonString);
 
@@ -19,14 +17,14 @@ const scrape = () => {
 		throw new Error('No data returned from scraper');
 	}
 
-	const reviews: Review[] = extractThumbtackData(reviewJson);
+	const reviews: ThumbtackReview[] = extractThumbtackData(reviewJson);
 
-  if (reviews.length === 0 || !reviews) {
-    throw new Error('No data returned from scraper');
-  }
-  console.log('reviews', reviews);
+	if (reviews.length === 0 || !reviews) {
+		throw new Error('No data returned from scraper');
+	}
+	console.log('reviews', reviews);
 
-  return reviews;
+	return reviews;
 };
 
 export default scrape;
